53a4faf58bbd0b79a600a2473f3a5923150bca82
C0B23030 <c0b230305c@edu.teu.ac.jp>
2024-06-11 16:08:35 +0900

    ５まで修正版

--- pygame_image.py
+++ pygame_image.py
@@ -18,7 +18,9 @@ def main():
             if event.type == pg.QUIT: return
 
         screen.blit(bg_img, [0, 0])
-        screen.blit(kktn_img, [300, 200])
+        kktn_rct = kktn_img.get_rect()
+        kktn_rct.center = (300, 200)
+        screen.blit(kktn_img, kktn_rct)
         pg.display.update()
         tmr += 1        
         clock.tick(200)
