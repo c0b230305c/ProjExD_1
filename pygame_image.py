c848fa5c7243538d4631f63f7ece6bbe39e81b67
C0B23030 <c0b230305c@edu.teu.ac.jp>
2024-06-11 16:41:51 +0900

    練習7-2

--- pygame_image.py
+++ pygame_image.py
@@ -14,18 +14,23 @@ def main():
     x = 0
     kktn_img = pg.image.load("fig/3.png")
     kktn_img = pg.transform.flip(kktn_img, True, False)
+    bg_imgf = pg.transform.flip(bg_img, True, False)
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
         screen.blit(bg_img, [x, 0])
+        screen.blit(bg_imgf, [x+1600, 0])
+        screen.blit(bg_img, [x + 3200, 0] )
+        screen.blit(bg_imgf, [x + 4800, 0])
+
         kktn_rct = kktn_img.get_rect()
         kktn_rct.center = (300, 200)
         screen.blit(kktn_img, kktn_rct)
         pg.display.update()
         tmr += 1     
         x -= 1
-        if x == -800:
+        if x == -3200:
             x = 0   
         clock.tick(200)
 
